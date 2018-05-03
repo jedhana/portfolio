8a4456778f42d1baba59d4e641b40333f6413214
jeremy dhanani <Jeremy.dhanani@gmail.com>
2018-05-03 17:20:19 +0100

    javascript implementation : toggle hidden content || css fixed menu in media queries

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,3 +2,27 @@
 //= require bootstrap
 //= require_tree .
 
+
+
+// document.getElementById('codediv').onClick=function(){
+//   document.getElementById('coding-content').style.display='block';
+// };
+
+
+// function initElement() {
+//   var p = document.getElementById("codediv");
+//     // NOTE: showAlert(); or showAlert(param); will NOT work here.
+//     // Must be a reference to a function name, not a function call.
+//     p.onclick = showCode;
+//   };
+
+// function showCode(event) {
+//   document.getElementById('coding-content').style.display='block';
+// }
+
+const button = document.getElementById("codediv");
+const codecontent = document.getElementById("coding-content");
+
+button.onclick = function(){
+  codecontent.className = "open";
+};
