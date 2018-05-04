a4d8ed4eb42f76ac45265cad92ce8589599eedc1
jeremy dhanani <Jeremy.dhanani@gmail.com>
2018-05-04 16:10:03 +0100

    slide up

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -3,26 +3,24 @@
 //= require_tree .
 
 
+// import Typed from 'typed.js'
+//   const typed = new Typed(".typed", {
+//     strings: ["Jeremy Dhanani", "[JEDHANA]"],
+//     typeSpeed: 50,
+//     backSpeed: 30,
+//   });
 
-// document.getElementById('codediv').onClick=function(){
-//   document.getElementById('coding-content').style.display='block';
-// };
 
-
-// function initElement() {
-//   var p = document.getElementById("codediv");
-//     // NOTE: showAlert(); or showAlert(param); will NOT work here.
-//     // Must be a reference to a function name, not a function call.
-//     p.onclick = showCode;
-//   };
-
-// function showCode(event) {
-//   document.getElementById('coding-content').style.display='block';
-// }
-
+// CONTENT TOGGLE BELOW
 const button = document.getElementById("codediv");
 const codecontent = document.getElementById("coding-content");
 
-button.onclick = function(){
-  codecontent.className = "open";
-};
+if (button){
+  button.onclick = function(){
+    codecontent.className = "open";
+  };
+}
+
+// $('.codediv, .coding-content').click(function() {
+//   $('.coding-content').toggleClass('close');
+// });
